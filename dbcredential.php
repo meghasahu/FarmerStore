5fb3fb820e520bed920ae8b075e842430c72d194
meghasahu <megha.suresh.sahu@gmail.com>
2018-11-02 16:14:57 +0530

    Update dbcredential.php

--- dbcredential.php
+++ dbcredential.php
@@ -5,7 +5,7 @@ define ('DB_PASSWORD','megha@1710');
 define ('DB_HOST','us-cdbr-iron-east-02.cleardb.net');
 define ('DB_NAME','gcp_dc6441c29cece88cb2a6');
 
-$conn = new mysqli(DB_HOST,DB_PASSWORD,DB_USER,DB_NAME);
+$conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
   // Check connection
   if ($conn->connect_error) {
       die('Connection failed: ' . $conn->connect_error);

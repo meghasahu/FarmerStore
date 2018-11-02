1b39ce05eb93f277d3ac1328ca9cffa0e6b1fb0b
meghasahu <megha.suresh.sahu@gmail.com>
2018-11-02 17:20:41 +0530

    Update createdb.php

--- createdb.php
+++ createdb.php
@@ -1,16 +1,16 @@
 <?php
 
-require 'dbcredential.php';
+//require 'dbcredential.php';
 
 echo "done";
 
-$query = "CREATE TABLE admin (admin_id varchar(50) NOT NULL primary key,password varchar(100) NOT NULL);";
-$result = mysqli_query($conn,$query) or die("cannot create execute");
+//$query = "CREATE TABLE admin (admin_id varchar(50) NOT NULL primary key,password varchar(100) NOT NULL);";
+//$result = mysqli_query($conn,$query) or die("cannot create execute");
 
-$pass = md5("megha");
- $query = "insert into admin values("2016.megha.sahu@ves.ac.in",$pass);";
+//$pass = md5("megha");
+ //$query = "insert into admin values("2016.megha.sahu@ves.ac.in",$pass);";
 
- $result = mysqli_query($conn,$query) or die("cannot insert execute");
+ //$result = mysqli_query($conn,$query) or die("cannot insert execute");
 
 
 ?>

c428c9974200238658f0683cd9d11701231c94e7
meghasahu <megha.suresh.sahu@gmail.com>
2018-11-02 17:05:54 +0530

    Update createdb.php

---
+++ createdb.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'dbcredential.php';
+
+$query = "CREATE TABLE admin (admin_id varchar(50) NOT NULL primary key,password varchar(100) NOT NULL);";
+ $result = mysqli_query($conn,$query) or die("cannot create execute");
+
+$pass = md5("megha");
+ $query = "insert into admin values("2016.megha.sahu@ves.ac.in",$pass);";
+
+ $result = mysqli_query($conn,$query) or die("cannot insert execute");
+
+
+?>
